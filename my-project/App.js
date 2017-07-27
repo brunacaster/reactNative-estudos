d3cc15b3239228fcaa2c30ce59a4bc49b55df2e9
Bruna Almeida <brunacaster@gmail.com>
2017-07-27 12:41:36 +0100

    fadein animation on background image

---
+++ my-project/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Splash from './Splash.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Splash />
+    );
+  }
+}
---
+++ my-project/Splash.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Animated, Easing } from 'react-native';
+import FadeInView from './FadeInView.js';
+
+export default class Splash extends React.Component {
+    render() {
+        return (
+
+            <View style={styles.container}>
+
+                <FadeInView style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    }}>
+                    <Image style={styles.backgroundImage}
+                    source={require('./Background.jpg')}
+                    />
+                </FadeInView>
+
+                <Text style={styles.titlePage}>Hello!</Text>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        height: '100%',
+        width: '100%',
+    },
+    titlePage: {
+      fontSize: 80,
+      color: '#fff',
+    }
+});
